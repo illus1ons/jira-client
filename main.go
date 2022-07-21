9a3b6731f9d25ef599489524369bf53f2a69ca8d
Youngbae Yoo <rcondition81@gmail.com>
2022-07-21 23:22:02 +0900

    Add 이슈 목록 조회 기능 추가
    Jira JQL 을 이용한 조회 기능

--- main.go
+++ main.go
@@ -25,12 +25,12 @@ func main() {
 
 	me, _, err := client.User.GetSelf()
 	if err != nil {
-		log.Fatalln("getSelf")
+		log.Fatalln(err)
 	}
-
 	log.Println(me)
 
 	getProjects(client)
+	getIssues(client, "project = 'ET Taxtron'")
 }
 
 // 모든 프로젝트 목록을 가져오는 함수
@@ -42,5 +42,21 @@ func getProjects(client *jira.Client) {
 
 	for _, project := range *projectList {
 		fmt.Printf("%s: %s\n", project.Key, project.Name)
+	}
+}
+
+// Jira JQL 문자열을 이용하여 이슈 조회
+func getIssues(client *jira.Client, jql string) {
+	issues, _, err := client.Issue.Search(jql, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, i := range issues {
+		fmt.Printf("%s (%s/%s): %+v -> %s\n", i.Key, i.Fields.Type.Name, i.Fields.Priority.Name, i.Fields.Summary, i.Fields.Status.Name)
+		if i.Fields.Assignee != nil {
+			fmt.Printf("Assignee : %v\n", i.Fields.Assignee.DisplayName)
+		}
+		fmt.Printf("Reporter: %v\n", i.Fields.Reporter.DisplayName)
 	}
 }
